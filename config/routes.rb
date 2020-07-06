1edc1796bfed4076788c5c1983dce75778bba2db
adam <aabou9@gmail.com>
2020-07-06 15:04:04 +0100

    add name, username to devise#user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    root to: 'devise/registrations#new'
-  end
+  devise_for :users
+  #  do
+  #   root to: 'devise/registrations#new'
+  # end
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
