18889b1f2795f0f841435f19145098485a68739d
adam <aabou9@gmail.com>
2020-07-06 14:09:49 +0100

    fix devise root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    root to: 'devise/registrations#new'
+  end
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
