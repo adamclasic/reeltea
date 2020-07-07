b9b6ac799a49f18081e1c02d76e7e9e15c7aa39e
adam <aabou9@gmail.com>
2020-07-07 17:44:57 +0100

    add views of user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  #  do
-  #   root to: 'devise/registrations#new'
-  # end
+
   resources :tweets
-    root to: 'tweets#index'
+  resources :users, only: [:show, :index]
+  root to: 'tweets#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
