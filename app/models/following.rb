d86d834dd00a5584e277a654e770c7916b274caa
adam <aabou9@gmail.com>
2020-07-07 21:25:44 +0100

    add following relations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,5 @@
 class Following < ApplicationRecord
+    belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
+    belongs_to :following, class_name: 'User', foreign_key: 'followed_id'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :tweets, class_name: 'Tweet', foreign_key: 'author_id'
+  
+  has_many :followers_ids, class_name: 'Following', foreign_key: 'followed_id'
+  has_many :followers, through: :followers_ids, source: 'follower'
+  
+  has_many :followeds_ids, class_name: 'Following', foreign_key: 'follower_id'
+  has_many :followeds, through: :followeds_ids, source: 'following'
+
+  # has_many :followeds, class_name: 'Following', foreign_key: 'followed_id'
 end
