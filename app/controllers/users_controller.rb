997aa5e1d5cf728324afc1ed4915c8ca14aa82db
adam <aabou9@gmail.com>
2020-07-07 17:37:51 +0100

    add user#show, user#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        # @user_tweets = @user.tweets
+        # @user_follwers = @user.follwers
+        # @user_followings = @user.followings
+    end
+
+    def index
+        @all_users = User.all
+    end 
 end
