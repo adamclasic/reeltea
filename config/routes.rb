25d101e33d2dd7cb5fea483b6fdb775bb5ec9e95
adam <aabou9@gmail.com>
2020-07-07 16:26:08 +0100

    fix redirecting issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   #   root to: 'devise/registrations#new'
   # end
   resources :tweets
+    root to: 'tweets#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
