f0103f9fc65b5e49c0940da21187f848a626d52d
adam <aabou9@gmail.com>
2020-07-07 17:33:31 +0100

    add controller User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ protected
   def configure_permitted_parameters
 
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
 
   end
 end
