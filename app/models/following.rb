61ec8d69d1869fbd253010bd5116f4f52938c559
adam <aabou9@gmail.com>
2020-07-08 12:11:22 +0100

    add active storage

--- app/models/following.rb
+++ app/models/following.rb
@@ -2,7 +2,5 @@ class Following < ApplicationRecord
   belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
   belongs_to :following, class_name: 'User', foreign_key: 'followed_id'
 
-  validates_uniqueness_of :user_name, scope: :account_id
-
 
 end
