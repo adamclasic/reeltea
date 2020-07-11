0e0d490d9d0c625eff7e81ec2c5722ca0f380211
adam <aabou9@gmail.com>
2020-07-11 20:33:42 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
 
   resources :tweets
   resources :users, only: %i[show index]
-  get 'follow/:id', to: 'users#followship'
-  delete 'follow/:id', to: 'users#unfollowship'
-  resources :tweets, only: %i[create destroy]
+  resources :followings, only: %i[create destroy]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
