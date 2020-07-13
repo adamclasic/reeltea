699153a2e647d681fc33e24fcc10a837a95c1781
adam <aabou9@gmail.com>
2020-07-13 16:55:44 +0100

    fix n+1 issue in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @user_tweets = @user.tweets.includes(:author)
-    @user_followers = @user.followers#.includes(:follower)
-    @user_followeds = @user.followeds#.includes(:following)
+    @user = User.includes(image_attachment: :blob).includes(:image_attachment).includes(:cover_attachment).includes(:followers).includes(followeds: [:image_attachment]).includes(followers: [:image_attachment]).includes(:tweets).includes(tweets: [:author]).find(params[:id])
+    @user_tweets = @user.tweets#.includes(:author)
+    @user_followers = @user.followers#.includes(image_attachment: :blob).includes(:image_attachment).includes(:followers)
+    @user_followeds = @user.followeds#.includes(image_attachment: :blob).includes(:image_attachment).includes(:followers)
     @tweet = Tweet.new
   end
 
   def index
     @all_users = User.all
   end
+
+
 
 end
