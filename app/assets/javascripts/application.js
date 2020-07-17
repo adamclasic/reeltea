0886b3515c1bbc9a9ea9f8b4ded5044da179725f
adam <aabou9@gmail.com>
2020-07-17 21:15:07 +0100

    add responsive sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,27 @@
 //= require bootstrap
 //= require_tree .
 
-$(document).on('click', 'someyourContainer .dropdown-menu', function (e) {
-    e.stopPropagation();
+$(document).ready(function () {
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
   });
 
-   $('.dropdown-toggle').dropdown()
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+});
+
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+});
+
+$('#sidebarCollapse').on('click', function () {
+   // open or close navbar
+   $('#sidebar').toggleClass('active');
+   // close dropdowns
+   $('.collapse.in').toggleClass('in');
+   // and also adjust aria-expanded attributes we use for the open/closed arrows
+   // in our CSS
+   $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+});
+});
