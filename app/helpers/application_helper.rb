67b0081f65337039302a953b02cc87595dca77a3
adam <aabou9@gmail.com>
2020-07-17 15:11:03 +0100

    add tweet vertical card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,6 +23,14 @@ module ApplicationHelper
     end
   end
 
+  def print_image_squar(user)
+    if user.image.attachment
+      image_tag(user.image, size: '55x55', class: 'rounded')
+    else
+      image_tag('avatar.png', size: '55x55', class: 'rounded')
+    end
+  end
+
   def print_follow_btn(user)
     unless current_user?(user)
       if following?(user)
@@ -50,12 +58,18 @@ module ApplicationHelper
   end
 
   def render_following_users(user_following_list)
-
     if !user_following_list.empty?
         render 'users/follows', list:  user_following_list
     else
-        "this user has no followers yet."
+        "No one to display."
     end
+  end
 
+  def print_tweet_btns(tweet)
+    if current_user?(tweet.author)
+      content_tag :td do link_to 'Edit', edit_tweet_path(tweet) end 
+      content_tag :td do link_to 'Destroy', tweet, method: :delete, data: { confirm: 'Are you sure?' } end 
+    end
+      content_tag :td do link_to 'Show', tweet end
   end
 end
