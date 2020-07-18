1167dd941a0596e9c15fee71e748f54259e2b676
adam <aabou9@gmail.com>
2020-07-18 11:55:40 +0100

    add style to flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,26 +21,28 @@
 //= require_tree .
 
 $(document).ready(function () {
-
+  
   $('#sidebarCollapse').on('click', function () {
       $('#sidebar').toggleClass('active');
   });
 
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
-});
+  });
 
   $("#sidebar").mCustomScrollbar({
     theme: "minimal"
-});
+  });
 
-$('#sidebarCollapse').on('click', function () {
-   // open or close navbar
-   $('#sidebar').toggleClass('active');
-   // close dropdowns
-   $('.collapse.in').toggleClass('in');
-   // and also adjust aria-expanded attributes we use for the open/closed arrows
-   // in our CSS
-   $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-});
+  $('#sidebarCollapse').on('click', function () {
+    // open or close navbar
+    $('#sidebar').toggleClass('active');
+    // close dropdowns
+    $('.collapse.in').toggleClass('in');
+    // and also adjust aria-expanded attributes we use for the open/closed arrows
+    // in our CSS
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+  // $.alert("Alert Message","Alert Title")
+
 });
